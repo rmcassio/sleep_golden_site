8a2b4290674fea28f9ff3b24ed8c128223eb0fd9
rmcassio <cassio_r.m@hotmail.com>
2024-06-17 09:19:19 -0300

    Updates

---
+++ _app/immutable/entry/start.6DQ6aur_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BT2VtvKn.js";export{t as start};
--- output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DqUJPvzM.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.CV5QbAHm.js"];
-export const stylesheets = ["_app/immutable/assets/2.B1FRlR9K.css","_app/immutable/assets/app.BZ0D1hJr.css"];
-export const fonts = [];
